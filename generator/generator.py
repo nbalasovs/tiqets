b2eeefbf9396ea6627e4db94ddd5e1130d1036a0
Nikaels Balasovs <balashovnikael@gmail.com>
2023-04-18 01:15:05 +0200

    Created Generator class and updated main function

--- generator/generator.py
+++ generator/generator.py
@@ -1,4 +1,14 @@
+import sys
 from argparse import Namespace
+from pandas import DataFrame
+from .helpers import read_csv_file, remove_duplicates
+
+
+class Generator:
+    def __init__(self, orders: DataFrame, barcodes: DataFrame, output_file_path: str) -> None:
+        self.orders = orders
+        self.barcodes = barcodes
+        self.path = output_file_path
 
 
 def main(ns: Namespace) -> None:
@@ -7,4 +17,9 @@ def main(ns: Namespace) -> None:
     :param ns: argparse namespace
     :return:
     """
-    pass
+    orders = read_csv_file(ns.orders)
+    barcodes = read_csv_file(ns.barcodes)
+    deduplicated_barcodes, duplicates = remove_duplicates(barcodes, "barcode")
+
+    for duplicate in duplicates:
+        print(f"duplicate barcode: {int(duplicate)}", sys.stderr)
