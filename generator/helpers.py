c0599a5e2a0847c137db346ed4537390de1d9e8b
Nikaels Balasovs <balashovnikael@gmail.com>
2023-04-18 01:07:48 +0200

    Added multiple helper functions and unittests

--- generator/helpers.py
+++ generator/helpers.py
@@ -1,4 +1,5 @@
 from pandas import read_csv, DataFrame
+from typing import Tuple
 
 
 def read_csv_file(path: str) -> DataFrame:
@@ -8,3 +9,37 @@ def read_csv_file(path: str) -> DataFrame:
     :return: pandas dataframe
     """
     return read_csv(path)
+
+
+def flatten(items: list) -> list:
+    """
+    Flattens list
+    :param items: original list
+    :return: flattened list
+    """
+    flattened = []
+    for item in items:
+        for elem in item:
+            flattened.append(elem)
+    return flattened
+
+
+def remove_duplicates(data: DataFrame, key: str) -> Tuple[DataFrame, list]:
+    """
+    Removes duplicates from dataframe
+    :param data: original dataframe
+    :param key: column key based on which duplicates should be removed
+    :return: cleaned dataframe and a list of duplicates
+    """
+    seen = {}
+    for idx, item in data.iterrows():
+        value = item[key]
+        if value not in seen:
+            seen[value] = [idx]
+        else:
+            seen[value].append(idx)
+
+    duplicates = {k: v for k, v in seen.items() if len(v) > 1}
+    indexes = flatten(list(duplicates.values()))
+
+    return data.drop(indexes).reset_index(drop=True), list(duplicates.keys())
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,7 @@
 import unittest
 
-from generator.helpers import read_csv_file
+from pandas import DataFrame
+from generator.helpers import read_csv_file, flatten, remove_duplicates
 
 
 class TestHelpers(unittest.TestCase):
@@ -16,3 +17,31 @@ class TestHelpers(unittest.TestCase):
             read_csv_file(self.correct_file_path)
         except FileNotFoundError:
             self.fail("exception was raised with correct file path")
+
+    def test_flatten_empty_list(self):
+        output = flatten([])
+        correct_output = []
+        self.assertEqual(correct_output, output)
+
+    def test_flatten_one_element(self):
+        output = flatten([[1, 2, 3]])
+        correct_output = [1, 2, 3]
+        self.assertEqual(correct_output, output)
+
+    def test_flatten_multiple_elements(self):
+        output = flatten([[1, 2], [3, 4]])
+        correct_output = [1, 2, 3, 4]
+        self.assertEqual(correct_output, output)
+
+    def test_remove_duplicates_no_duplicates(self):
+        df = DataFrame([[1, 2], [3, 4]], columns=["one", "two"])
+        output, duplicates = remove_duplicates(df, "one")
+        self.assertTrue(df.equals(output))
+        self.assertEqual([], duplicates)
+
+    def test_remove_duplicates(self):
+        df = DataFrame([[1, 2], [1, 1], [3, 4]], columns=["one", "two"])
+        correct_df = DataFrame([[3, 4]], columns=["one", "two"])
+        output, duplicates = remove_duplicates(df, "one")
+        self.assertTrue(correct_df.equals(output))
+        self.assertEqual([1], duplicates)
